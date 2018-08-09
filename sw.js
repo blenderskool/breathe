a8eb27668f9eea7c7751e37e9c13e2364d3f1604
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-08-09 22:19:07 +0530

    Updated service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Use LocalForage to store, get data and use it when offline
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.1/localforage.min.js');
 
-var staicCacheName = 'breathe-static-v4';
+var staicCacheName = 'breathe-static-v5';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
