771889aa3a1f165fc983eef8f0d6d4ae079bafa5
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2019-06-09 17:33:38 +0530

    Fixes #8

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 // Use LocalForage to store, get data and use it when offline
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.1/localforage.min.js');
 
-var staicCacheName = 'breathe-static-v8';
+var staicCacheName = 'breathe-static-v9';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
